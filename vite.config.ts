24f0e1c568e40e2f85fd9ccaa5a0ac13a727588a
Filipe Costa <fcosta@fuze.com>
2022-03-01 15:51:53 +0000

    Module federation with vite-plugin-federation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [
+        federation({
+          name: 'react-plantuml-ide',
+          filename: 'react-plantuml-ide.js',
+          exposes: {
+            './Button': './src/ui/App.tsx',
+          },
+          // remotes:{
+          //   foo: 'remote_foo'
+          // },
+          // shared: ['vue']
+        })
+      ],
+    }
+  }
 })
