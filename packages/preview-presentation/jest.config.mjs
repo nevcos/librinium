75b5107c7e9fc71ab57fd54b7784a6311795b1c1
Filipe Costa <fcosta@fuze.com>
2022-04-04 16:50:12 +0100

    Single repo setup (WIP)
    Moved files to root + merged config files

--- packages/preview-presentation/jest.config.mjs
+++
@@ -1,6 +0,0 @@
-import {getDefaultJestConfig} from "../shared-dev/config/jest/defaultJestConfig.mjs";
-
-// noinspection JSUnusedGlobalSymbols
-export default {
-  ... getDefaultJestConfig()
-};
--- packages/preview-presentation/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import ReactDOM from "react-dom";
-
-import { PreviewPresentation } from "./ui/PreviewPresentation";
-
-ReactDOM.render(<PreviewPresentation />, document.getElementById("root"));
