5a9adc082a4f8df62562769125e137c2c2a3c302
Filipe Costa <fcosta@fuze.com>
2022-04-05 17:54:29 +0100

    Keep Remark preview in the same slide when updating

--- src/ui/previewRemark/PreviewRemark.tsx
+++ src/ui/previewRemark/PreviewRemark.tsx
@@ -1,11 +1,18 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
-import { useSelector } from 'react-redux';
-import { documentStoreSelectors } from '../../store/documentStore';
+import { useSelector } from "react-redux";
+import { documentStoreSelectors } from "../../store/documentStore";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const remark: any;
+
+interface RemarkSlide {
+    getSlideIndex(): number;
+    notes: string;
+    properties: Map<string, string>;
+    content: string;
+}
 
 const Container = styled.div`
   position: relative !important;
@@ -29,9 +36,16 @@ const Container = styled.div`
 export function PreviewPresentation() {
   const code = useSelector(documentStoreSelectors.getSelectedDocument)?.code;
   const container = useRef<HTMLDivElement | null>(null);
+  const currentSlide = useRef<number>();
 
   useEffect(() => {
-    remark.create({ container: container.current, source: code });
+    const slideshow = remark.create({ container: container.current, source: code });
+    slideshow.on("showSlide", (slide: RemarkSlide) => {
+      currentSlide.current = slide.getSlideIndex() + 1;
+    });
+    if (currentSlide.current) {
+      slideshow.gotoSlide(currentSlide.current);
+    }
     return () => {
       if (container.current) container.current.innerHTML = "";
     };
