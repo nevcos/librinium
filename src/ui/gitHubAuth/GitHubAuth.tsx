33996cf57ca4bea8a4de25bc6c644a8b6ec31c09
aneves <adn.denied@gmail.com>
2022-04-07 16:10:16 +0100

    Updated GitHubAuth

--- src/ui/gitHubAuth/GitHubAuth.tsx
+++ src/ui/gitHubAuth/GitHubAuth.tsx
@@ -1,13 +1,9 @@
-import { useDataSource } from "../../hook/useDataSource";
-import { GITHUB_CLIENT_ID, GITHUB_TOKEN_COOKIE_KEY } from "../../remoteApi/githubApi";
-import { cookieResource } from "../../util/resource";
+import { GITHUB_CLIENT_ID } from "../../remoteApi/githubApi";
 
 export function GitHubAuth(): JSX.Element {
-  const token = useDataSource(cookieResource(GITHUB_TOKEN_COOKIE_KEY));
-
   function auth() {
     window.location.href = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}`;
   }
 
-  return <>{!token && <button onClick={() => auth()}>GHAuth</button>}</>;
+  return <button onClick={() => auth()}>GHAuth</button>;
 }
--- src/util/resource.ts
+++
@@ -1,9 +0,0 @@
-import Cookies from "js-cookie";
-
-export const localStorageResource = (key: string) => () => {
-  return localStorage.getItem(key);
-};
-
-export const cookieResource = (key: string) => () => {
-  return Cookies.get(key);
-};
