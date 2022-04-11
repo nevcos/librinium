31fb9d6f5850302f3fd3d933eb4a0ffdc164369e
Filipe Costa <fcosta@fuze.com>
2022-04-11 17:50:20 +0100

    Fixed type error

--- src/ui/shared/useActiveGist.ts
+++ src/ui/shared/useActiveGist.ts
@@ -2,7 +2,6 @@ import {useParams} from "react-router-dom";
 
 import {useGistSelector} from "../../hook/useGistSelector";
 import {DocumentId} from "../../domain/document/DocumentId";
-import {DocumentStoreState} from "../../domain/documentStoreState/DocumentStoreState";
 import {Document} from "../../domain/document/Document";
 import {documentStoreSelectors} from "../../store/documentStore";
 
@@ -12,6 +11,6 @@ type Params = {
 
 export function useActiveGist(): {gistId: DocumentId, gist: Document | null} {
   const gistId = useParams<Params>().gistId as DocumentId;
-  const gist = useGistSelector<DocumentStoreState, Document | null>(state => documentStoreSelectors.getDocument(state, gistId));
+  const gist = useGistSelector<Document | null>(state => documentStoreSelectors.getDocument(state, gistId));
   return {gistId, gist};
 }
