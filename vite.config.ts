9c0ff2216a5e898e2a2ac3cbbe07b47b43aa514b
Filipe Costa <fcosta@fuze.com>
2022-04-13 12:41:25 +0100

    Fix URLs with extension not working in ViteJS dev server

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import {defineConfig} from "vite";
+import {defineConfig, ViteDevServer} from "vite";
 import react from "@vitejs/plugin-react";
+import rewriteAll from "vite-plugin-rewrite-all";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    // Supports SPA rewrite even when it includes an extension like /gists/test.md
+    rewriteAll()
   ]
 });
